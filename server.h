fe45954e743fff1369077f248926c6551b87e6a5
Calvin T VanWormer <ctvanwo@lps.umd.edu>
2023-07-14 14:09:46 -0400

    rework server.h

--- server.h
+++ server.h
@@ -2,6 +2,7 @@
 
 #define MAXWORKERS 10
 
+int findWorker(char *host, struct workers* arr);
 int populate_workers(
     struct workers* worker_array,
     int *p_n_workers,
@@ -50,16 +51,19 @@ void* processRequest(void* args)
             //     execvp(command, arguments);
             // }
             //sleep(1);
-            
-            s_send(worker_array[idx].work, "Worker populated");
 
-            printf("host stored: %s\n", worker_array[idx].host);
+            int index = findWorker(header[3], worker_array);
+            if(index == -1){
+                printf("Error finding worker");
+                exit(0);
+            }
+            s_send(worker_array[index].work, "Worker populated");
 
             //Creates thread to check for worker heartbeat
             struct ThreadArgs args;
             args.w_arr = worker_array;
-            args.socket = worker_array[idx].hb;
-            args.host = worker_array[idx].host;
+            args.socket = worker_array[index].hb;
+            args.host = worker_array[index].host;
 
             pthread_t thread_id;
             pthread_create(&thread_id, NULL, checkForUpdate, (void*)&args);
@@ -86,6 +90,17 @@ void* processRequest(void* args)
         }
     }
     free(header);
+}
+
+int findWorker(char *host, struct workers* arr){
+    int n_workers = getNumberElements(arr);
+    int i;
+    for(i=0; i<n_workers;i++){
+        if(strcmp(arr[i].host, host)==0){
+            return i;
+        }
+    }
+    return -1;
 }
 
 int populate_workers(
@@ -125,7 +140,7 @@ int populate_workers(
     sprintf(worker_addr, "tcp://%s:%d", worker_ip, port);
     void* hb = connect_socket(context, worker_addr);
 
-    int timeout = 7000; // 40 sec
+    int timeout = 40000; // 40 sec
     int opt = zmq_setsockopt(hb, ZMQ_RCVTIMEO, &timeout, sizeof(timeout));
     if(opt == -1){
         printf("Error setting opt");
